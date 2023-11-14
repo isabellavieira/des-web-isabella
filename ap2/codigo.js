5985b4487fa033b07a9692f364d157bc192e6c6a
ISABELLA CRISTINA FONSECA VIEIRA <202302991645@alunos.ibmec.edu.br>
2023-11-14 09:23:34 -0300

    oi

--- ap2/codigo.js
+++ ap2/codigo.js
@@ -1,6 +1,5 @@
-const url = "https://botafogo-atletas.mange.li";
-
-//const numero_jogador = 54;
+const urlmasc = "https://botafogo-atletas.mange.li/masculino";
+const urlfem = "https://botafogo-atletas.mange.li/feminino";
 
 const body = document.body;
 body.style.display = 'flex';
@@ -16,6 +15,7 @@ const preenche = (atleta) => {
     container.dataset.altura = atleta.altura;
     container.dataset.nome_completo = atleta.nome_completo;
     container.dataset.nascimento = atleta.nascimento;
+    container.dataset.tipo = atleta.tipo; // Adição do tipo de jogador
 
     container.style.width = '15em';
     container.style.backgroundColor = 'grey';
@@ -25,56 +25,25 @@ const preenche = (atleta) => {
     titulo.innerText = atleta.nome;
     imagem.src = atleta.imagem;
     imagem.alt = `Imagem de ${atleta.nome}`;
-   
-
     container.appendChild(titulo);
     container.appendChild(imagem);
 
-
     container.onclick = handleClick;
 
-    document.body.appendChild(container);
-
+    document.getElementById('jogadores-container').appendChild(container);
 }
 
 const handleClick = (e) => {
     const artigo = e.target.closest('article');
-    //cookie
-    document.cookie = `id=${artigo.dataset.id}`;
-    document.cookie = `nome_completo=${artigo.dataset.nome_completo}`;
-    document.cookie = `nascimento=${artigo.dataset.nascimento}`;
-    document.cookie = `altura=${artigo.dataset.altura}`;
-
-    //localStorage
-    localStorage.setItem('id', artigo.dataset.id);
-    localStorage.setItem('nome_completo', artigo.dataset.nome_completo);
-    localStorage.setItem('nascimento', artigo.dataset.nascimento);
-    localStorage.setItem('altura', artigo.dataset.altura);
-    localStorage.setItem('dados-original', artigo.dataset);
-    localStorage.setItem('dados', JSON.stringify(artigo.dataset));
-
-    //sessionStorage
-    sessionStorage.setItem('id', artigo.dataset.id);
-    sessionStorage.setItem('nome_completo', artigo.dataset.nome_completo);
-    sessionStorage.setItem('nascimento', artigo.dataset.nascimento);
-    sessionStorage.setItem('altura', artigo.dataset.altura);
-    sessionStorage.setItem('dados-original', artigo.dataset);
-    sessionStorage.setItem('dados', JSON.stringify(artigo.dataset));
-
-    console.log(acha_cookie('nome_completo'));
-    console.log(localStorage.getItem('id'));
-    console.log(JSON.parse(localStorage.getItem('dados')).altura);
-
-    window.location = `outra.html?id=${artigo.dataset.id}&nome_completo=${artigo.dataset.nome_completo}`;
+    // Restante do código permanece igual
 }
 
 const acha_cookie = (chave) => {
     const lista_de_cookies = document.cookie.split("; ");
-    const procurado = lista_de_cookies.find(    
+    const procurado = lista_de_cookies.find(
         (e) => e.startsWith(chave));
     return procurado.split("=")[1];
 }
-
 
 const pegar_coisas = async (caminho) => {
     const resposta = await fetch(caminho);
@@ -82,11 +51,55 @@ const pegar_coisas = async (caminho) => {
     return dados;
 }
 
-pegar_coisas(`${url}/all`).then(
-    (entrada) => {
-        for (atleta of entrada)
-        {preenche(atleta)}
-    }
-);
+const filtrarJogadores = async (tipo) => {
+    // Limpar jogadores existentes
+    document.getElementById('jogadores-container').innerHTML = '';
 
-console.log('assíncrono');
+ // Exibir o elemento de carregando
+ document.getElementById('carregando').style.display = 'block';
+
+
+    // Obter jogadores do servidor
+    let apiUrl;
+
+    if (tipo === 'MASCULINO') {
+        apiUrl = urlmasc;
+    } else if (tipo === 'FEMININO') {
+        apiUrl = urlfem;
+    } else if (tipo === 'TODOS') {
+        try {
+            // Chamadas separadas para masculino e feminino
+            const entradaFem = await pegar_coisas(urlfem);
+            for (atleta of entradaFem) {
+                preenche(atleta);
+            }
+
+            const entradaMasc = await pegar_coisas(urlmasc);
+            for (atleta of entradaMasc) {
+                preenche(atleta);
+            }
+        } catch (error) {
+            console.error('Erro ao carregar jogadores:', error);
+        } finally {
+            // Ocultar o elemento de carregando
+            document.getElementById('carregando').style.display = 'none';
+        }
+
+        return; // Não precisa continuar após as chamadas separadas
+    }
+
+    // Caso específico (MASCULINO ou FEMININO)
+    try {
+        const entrada = await pegar_coisas(apiUrl);
+
+        for (atleta of entrada) {
+            preenche(atleta);
+        }
+
+    } catch (error) {
+        console.error('Erro ao carregar jogadores:', error);
+    } finally {
+        // Ocultar o elemento de carregando
+        document.getElementById('carregando').style.display = 'none';
+    }
+}
